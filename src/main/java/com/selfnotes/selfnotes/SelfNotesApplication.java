a2a7f005125f48fe0a94955e20d5c7b02b6eddcd
rpoddar <rajeshpoddar1504@gmail.com>
2023-10-13 11:33:23 +0530

    first commit

---
+++ src/main/java/com/selfnotes/selfnotes/SelfNotesApplication.java
@@ -0,0 +1,13 @@
+package com.selfnotes.selfnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selfnotes/selfnotes/SelfNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfnotes.selfnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
