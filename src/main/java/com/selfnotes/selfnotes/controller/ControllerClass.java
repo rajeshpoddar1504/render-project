872c1fff943d6264c7f659da82a08f0c667e39ed
rpoddar <rajeshpoddar1504@gmail.com>
2023-10-17 02:10:09 +0530

    cotroller modification

--- src/main/java/com/selfnotes/selfnotes/controller/ControllerClass.java
+++ src/main/java/com/selfnotes/selfnotes/controller/ControllerClass.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ControllerClass {
 	
-	@GetMapping("/")
+	@GetMapping("/home")
 	public String getHome() {
 		return "home";
 	}
